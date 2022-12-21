dc41f2579dbb4220fd32cb350a558298d5a9ed24
= <=>
2022-12-21 15:36:57 +0400

    add search highlight feature

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -1,51 +1,78 @@
 import React from 'react';
 import { useUsers } from '../context/MainContext';
 export default function Table() {
-  const { users, search, setList } = useUsers();
+    const { users, search, setList } = useUsers();
 
-  const filteredUser = users.filter(user => JSON.stringify(user).toString().toLowerCase().includes(search.toString().toLowerCase())
-  );
-  
-  const sortName = () => {
-    setList(users?.sort((a, b) => a.name.localeCompare(b.name)));
-    console.log(users);
-  };
-  const sortEmail = () => {
-    setList(users?.sort((a, b) => a.email.localeCompare(b.email)));
-  };
+    const filteredUser = users.filter((user) =>
+        JSON.stringify(user)
+            .toString()
+            .toLowerCase()
+            .includes(search.toString().toLowerCase())
+    );
 
-  return (
-    <>
-      <div className="res-table">
-        <table className="table">
-          <thead className="t-head">
-            <tr>
-              <th>ID</th>
-              <th>
-                Name <button onClick={sortName}>sort</button>{' '}
-              </th>
-              <th>
-                Email <button onClick={sortEmail}>sort</button>
-              </th>
-              <th>Phone</th>
-              <th>Adress</th>
-            </tr>
-          </thead>
-          <tbody>
-            {filteredUser?.map((user, id) => (
-              <tr key={id}>
-                <td>{user.id}</td>
-                <td>{user.name}</td>
-                <td>{user.email}</td>
-                <td>{user.phone}</td>
-                <td>
-                  {user.address.city} - {user.address.street}
-                </td>
-              </tr>
-            ))}
-          </tbody>
-        </table>
-      </div>
-    </>
-  );
+    const highlightText = (text, highlight) => {
+        const parts = text.split(new RegExp(`(${highlight})`, 'gi'));
+        return (
+            <>
+                {parts.map((part, i) => (
+                    <span
+                        key={i}
+                        style={
+                            part.toLowerCase() === highlight.toLowerCase()
+                                ? { fontWeight: 'bold', background: 'yellow' }
+                                : {}
+                        }
+                    >
+                        {part}
+                    </span>
+                ))}
+            </>
+        );
+    };
+
+    const sortName = () => {
+        setList(users?.sort((a, b) => a.name.localeCompare(b.name)));
+        console.log(users);
+    };
+    const sortEmail = () => {
+        setList(users?.sort((a, b) => a.email.localeCompare(b.email)));
+    };
+
+    return (
+        <>
+            <div className="res-table">
+                <table className="table">
+                    <thead className="t-head">
+                        <tr>
+                            <th>ID</th>
+                            <th>
+                                Name <button onClick={sortName}>sort</button>{' '}
+                            </th>
+                            <th>
+                                Email <button onClick={sortEmail}>sort</button>
+                            </th>
+                            <th>Phone</th>
+                            <th>Adress</th>
+                        </tr>
+                    </thead>
+
+                    {/* Table Body */}
+                    <tbody>
+                        {filteredUser?.map((user, id) => (
+                            <tr key={id}>
+                                <td>{id + 1}</td>
+                                <td>{highlightText(user.name, search)}</td>
+                                <td>{highlightText(user.email, search)}</td>
+                                <td>{highlightText(user.phone, search)}</td>
+                                <td>
+                                    {highlightText(user.address.city, search)} -
+                                    {highlightText(user.address.street, search)}
+                                </td>
+                            </tr>
+                        ))}
+                    </tbody>
+                </table>
+            </div>
+        </>
+    );
 }
